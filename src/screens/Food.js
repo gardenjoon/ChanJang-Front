bf19126c2a17088eb27f58906fd89eb47dabd562
gardenjoon <wees114@gmail.com>
2021-10-02 14:11:11 +0900

    test2

--- src/screens/Food.js
+++ src/screens/Food.js
@@ -5,7 +5,7 @@ class FoodScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>test</Text>
+                <Text>test2</Text>
             </View>
         );
     }
