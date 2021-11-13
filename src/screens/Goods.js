7f794101d12afebb39f52a7d547e002838b177f0
gardenjoon <wees114@gmail.com>
2021-11-13 11:18:43 +0900

    기능 : axios 샘플코드

--- src/screens/Goods.js
+++ src/screens/Goods.js
@@ -1,12 +1,48 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
 
 const GoodsScreen = () => {
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchUsers = async () => {
+        try {
+            setError(null);
+            setUsers(null);
+            setLoading(true);
+            const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/users'
+            );
+            setUsers(response.data);
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false);
+        };
+    
+        fetchUsers();
+    }, []);
+
+    if (loading) return <Text>로딩중..</Text>;
+    if (error) return <Text>에러가 발생했습니다</Text>;
+    if (!users) return null;
     return (
-        <View style={styles.container}>
-            <Text>생활용품!</Text>
-        </View>
+      <View style={styles.container}>
+        {users.map(user => (
+          <Text >
+            {user.username}
+          </Text>
+        ))}
+      </View>
     );
+    // return (
+    //     <View style={styles.container}>
+    //         <Text>생활용품!</Text>
+    //     </View>
+    // );
 }
 
 const styles = StyleSheet.create({
